7e69f05475fa956287e79931c0ea91f1ba56f6af
Adam Leonard <115667156+Adaleo314@users.noreply.github.com>
2023-03-03 17:26:44 -0500

    Update variables.tf

--- terraform-two-tier/variables.tf
+++ terraform-two-tier/variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
 variable "vpc_name" {
   type    = string
   default = "new_vpc"
@@ -13,22 +8,46 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "private_subnets" {
-  default = {
-    "private_subnet_1" = 1
-    "private_subnet_2" = 2
-    "private_subnet_3" = 3
-  }
+variable "public_subnet_cidr" {
+  description = "Public Subnet cidr block"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "public_subnets" {
-  default = {
-    "public_subnet_1" = 1
-    "public_subnet_1" = 2
-    "public_subnet_3" = 3
-  }
+variable "private_subnet_cidr" {
+  description = "Private Subnet cidr block"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
+#EC2 Instance Info
+variable "ami" {
+  description = "ami of ec2 instance"
+  type        = string
+  default     = "ami-0557a15b87f6559cf"
+}
 
+variable "instance_type" {
+  description = "Size of EC2 Instances"
+  type        = string
+  default     = "t2.micro"
+}
 
+variable "user_data" {
+  description = "boostrap script for apache"
+  type        = string
+  default     = <<EOF
+    #!/bin/bash
+    
+    # Install Apache
+    sudo apt-get update -y
+    sudo apt upgrade -y
+    sudo apt install -y apache2
+    sudo echo "<img src="https://www.memecreator.org/static/images/memes/5148085.jpg" width="800" height="600"> > /var/www/html/index.html
+  
+  # Start Apache
+    sudo systemctl enable apache2
+    sudo systemctl start apache2
+    EOF
+}
 
