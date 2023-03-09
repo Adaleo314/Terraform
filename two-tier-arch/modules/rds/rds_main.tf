264b566ff0f44073f5d94dfe2eb3b099bd29d387
Adam Leonard <115667156+Adaleo314@users.noreply.github.com>
2023-03-08 21:11:49 -0500

    created RDS.tf

--- two-tier-arch/modules/rds/rds_main.tf
+++ two-tier-arch/modules/rds/rds_main.tf
@@ -1,1 +1,27 @@
-New
+# RDS subnet group
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "rds"
+  count = 2
+  subnet_ids = [aws_subnet.private_subnet_cidr.id]
+
+  tags = { 
+     name = "rds_subnet"
+  }
+}
+
+
+# RDS database on mysql engine
+resource "aws_db_instance" "my_db" {
+  allocated_storage      = 10
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet.id
+  engine                 = var.db_engine
+  engine_version         = var.db_engine_version
+  instance_class         = var.db_instance_class
+  multi_az               = true
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = var.db_password
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.database_sg.id]
+}
+
